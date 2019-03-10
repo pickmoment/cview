1654634f2af2f1dad6c7f5ae45a2d126b23f7cb3
moongeun.kim <moongeun.kim@linecorp.com>
2019-03-10 23:56:09 +0900

    a

--- monkey_comics.py
+++ monkey_comics.py
@@ -10,7 +10,24 @@ comics_dict = {
     '드래곤볼': 'https://www.mkmk02.com/chapter.php?n=comics&t=768',
     '슬램덩크': 'https://www.mkmk02.com/chapter.php?n=comics&t=6399',
     '배가본드': 'https://www.mkmk02.com/chapter.php?n=comics&t=1686',
-    '암살교실': 'https://www.mkmk02.com/chapter.php?n=comics&t=2488'
+    '암살교실': 'https://www.mkmk02.com/chapter.php?n=comics&t=2488',
+    '도박마': 'https://www.mkmk02.com/chapter.php?n=comics&t=860',
+    '우주형제': 'https://www.mkmk02.com/chapter.php?n=comics&t=2486',
+    '20세기 소년': 'https://www.mkmk02.com/chapter.php?n=comics&t=4916',
+    '마스터 키튼': 'https://www.mkmk02.com/chapter.php?n=comics&t=1196',
+    '약속의 네버랜드': 'https://www.mkmk02.com/chapter.php?n=comics&t=2429',
+    '하이큐': 'https://www.mkmk02.com/chapter.php?n=comics&t=4148',
+    '사채꾼 우시지마': 'https://www.mkmk02.com/chapter.php?n=comics&t=2217',
+    '창천항로': 'https://www.mkmk02.com/chapter.php?n=comics&t=3458',
+    '나의 지구를 지켜줘': 'https://www.mkmk02.com/chapter.php?n=comics&t=5567',
+    '이누야시키': 'https://www.mkmk02.com/chapter.php?n=comics&t=2478',
+    '시티헌터': 'https://www.mkmk02.com/chapter.php?n=comics&t=2092',
+    '도박묵시록 카이지': 'https://www.mkmk02.com/chapter.php?n=comics&t=675',
+    '펌프킨 시저스': 'https://www.mkmk02.com/chapter.php?n=comics&t=3939',
+    '주식회사 천재패밀리': 'https://www.mkmk02.com/chapter.php?n=comics&t=3266',
+    '신세기 에반게리온': 'https://www.mkmk02.com/chapter.php?n=comics&t=2297',
+    '아키라': 'https://www.mkmk02.com/chapter.php?n=comics&t=5066',
+    'MIX 믹스': 'https://www.mkmk02.com/chapter.php?n=comics&t=6375'
 }
 
 class Api:
